e10808878a27d8cc71893eabaf2051abe0839d9c
Vojtech Kolencik <vojtech.kolencik@vista.com>
2024-01-20 20:54:37 +0100

    Print out monthly values

--- CycleFor.Tests/CycleForShould.cs
+++ CycleFor.Tests/CycleForShould.cs
@@ -1,3 +1,5 @@
+using Moq;
+
 namespace CycleFor.Tests;
 
 public class CycleForShould
@@ -5,12 +7,29 @@ public class CycleForShould
     [Theory]
     [InlineData(0, 100)]
     [InlineData(1, 110)]
+    [InlineData(2, 121)]
     public void CalculateInterest_GivenZeroMonths(int numberOfMonths, decimal expectedFinalAmount)
     {
-        var calculator = new InterestCalculator();
+        var calculator = new InterestCalculator(Mock.Of<IInterestCalculationObserver>());
 
         var interest = calculator.CalculateInterest(100, numberOfMonths, 10);
 
         Assert.Equal(expectedFinalAmount, interest);    
+    }
+
+    [Fact]
+    public void ObserveAmountAfterEachMonth()
+    {
+        var observerMock = new Mock<IInterestCalculationObserver>();
+        var capturedMonths = new List<int>();
+        var capturedAmounts = new List<decimal>();
+        observerMock.Setup(_ => _.MonthlyValue(Capture.In(capturedMonths), Capture.In(capturedAmounts)));
+        
+        var calculator = new InterestCalculator(observerMock.Object);
+
+        calculator.CalculateInterest(100, 2, 10);
+
+        Assert.Equal(new[] {1, 2}, capturedMonths);
+        Assert.Equal(new[] {110m, 121m}, capturedAmounts);
     }
 }
--- src/CycleFor/Program.cs
+++ src/CycleFor/Program.cs
@@ -4,20 +4,53 @@ public static class Program
 {
     public static void Main(string [] args)
     {
-        Console.WriteLine("hello");
+        Console.Write("Zadejte počáteční hodnotu na účtu: ");
+        var initialAmountInput = Console.ReadLine();
+        
+        if (!decimal.TryParse(initialAmountInput, out var initialAmount))
+        {
+            Console.Error.WriteLine("Neplatné číslo");
+            Environment.Exit(-1);
+        }
+
+        var calculator = new InterestCalculator(new ConsoleInterestCalculationObserver());
+        var finalAmount = calculator.CalculateInterest(initialAmount, 12, 6);
+
+        Console.WriteLine($"Konečná částka: {finalAmount:F2}");
+    }
+
+    private class ConsoleInterestCalculationObserver : IInterestCalculationObserver
+    {
+        public void MonthlyValue(int monthsPassed, decimal amount)
+        {
+            Console.Out.WriteLine($"Částka po {monthsPassed} měsících: {amount:F212345}");
+        }
     }
 }
 
 internal class InterestCalculator
 {
+    private readonly IInterestCalculationObserver _observer;
+
+    public InterestCalculator(IInterestCalculationObserver observer)
+    {
+        _observer = observer;
+    }
+
     public decimal CalculateInterest(decimal initialAmount, int numberOfMonths, int interestRate)
     {
         var amount = initialAmount;
         for (var monthsPassed = 1; monthsPassed <= numberOfMonths; monthsPassed++)
         {
             amount *= new decimal(100 + interestRate) / 100;
+            _observer.MonthlyValue(monthsPassed, amount);
         }
 
         return amount;
     }
+}
+
+public interface IInterestCalculationObserver
+{
+    void MonthlyValue(int monthsPassed, decimal amount);
 }
